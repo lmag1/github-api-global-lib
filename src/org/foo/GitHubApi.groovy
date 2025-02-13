40f0ac1bfa680cd2910a5f193e310ae4d45f8c69
lmag1 <magradzelevani4@gmail.com>
2025-02-14 01:00:32 +0400

    Added src and vars directories with example files

---
+++ src/org/foo/GitHubApi.groovy
@@ -0,0 +1,8 @@
+package org.foo
+
+class GitHubApi {
+    def getUserRepositories(String username) {
+        // GitHub API-ს გამოძახების ლოგიკა
+        return ["repo1", "repo2", "repo3"] // მაგალითისთვის
+    }
+}
