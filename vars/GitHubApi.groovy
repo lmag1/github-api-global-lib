ff369529f21e5b31bfae25178014e5c14b030916
lmag1 <magradzelevani4@gmail.com>
2025-02-14 00:47:19 +0400

    შესწორებულია GitHubApi.groovy

--- vars/GitHubApi.groovy
+++ vars/GitHubApi.groovy
@@ -1,18 +1,5 @@
-@Library('github-api-global-lib') _
-
-pipeline {
-    agent any
-
-    stages {
-        stage('Example') {
-            steps {
-                script {
-                    // გამოიყენეთ ბიბლიოთეკის ფუნქციები
-                    def api = new com.darinpope.github.GitHubAPI()
-                    def repos = api.getUserRepositories('darinpope')
-                    echo "Repositories: ${repos}"
-                }
-            }
-        }
+class GitHubApi {
+    def call() {
+        println "GitHub API Class Loaded!"
     }
 }
